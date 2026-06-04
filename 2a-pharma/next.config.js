988718d0344e14c4d9ece1e5829b3703c1a1cced
unknown <kostakuqo5@gmail.com>
2026-06-04 12:03:18 +0200

    fix nextjs basePath for github pages

--- 2a-pharma/next.config.js
+++ 2a-pharma/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-
-  basePath: "/2apharma",
-  assetPrefix: "/2apharma/",
+  images: { unoptimized: true }
 };
+module.exports = nextConfig;
 
 export default nextConfig;
