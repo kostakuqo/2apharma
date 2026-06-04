1ee2e75f94d0a7780ac00c5edba7bc7512cc4ad9
unknown <kostakuqo5@gmail.com>
2026-06-04 13:16:52 +0200

    work

--- 2a-pharma/next.config.mjs
+++ 2a-pharma/next.config.mjs
@@ -1,6 +1,9 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: "/2apharma",
+  assetPrefix: "/2apharma/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
