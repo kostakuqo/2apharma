4b0bc6bb9ec0175503d278049467a82baaccc68e
unknown <kostakuqo5@gmail.com>
2026-06-04 12:49:33 +0200

    work on progres

--- 2a-pharma/next.config.mjs
+++ 2a-pharma/next.config.mjs
@@ -1,12 +1,6 @@
-/** @type {import('next').NextConfig} */
-const repo = "/2apharma";
-
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-
-  basePath: repo,
-  assetPrefix: repo + "/",
 };
 
 export default nextConfig;
