03df7bd9f64bcfaf3f4077928d379fe695052abc
unknown <kostakuqo5@gmail.com>
2026-06-04 12:05:56 +0200

    fix next config for github pages export

---
+++ 2a-pharma/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+
+  basePath: "/2apharma",
+  assetPrefix: "/2apharma/",
+};
+
+export default nextConfig;
