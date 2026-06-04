303bd533e4896195f62f8bfd59b6a353f442c5fb
unknown <kostakuqo5@gmail.com>
2026-06-04 12:48:58 +0200

    Updates

--- _next/static/xU0oV9KUfViu-aLFCKGzf/_buildManifest.js
+++ _next/static/xU0oV9KUfViu-aLFCKGzf/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/2apharma//_next/:path+",
-        "destination": "/2apharma/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
